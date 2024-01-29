77378d3ff0ce3346fc29c69276a3967104503aee
harrison.kim(김하람)/kakaogames <harrison.kim@kakaocorp.com>
2024-01-29 13:45:57 +0900

    chat.html 추가

---
+++ scripts/login.js
@@ -0,0 +1,34 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkLogin() {
+    ldap = getCookie('coco_ldap');
+    if (ldap === '') {
+        needLogin();
+    }
+}
+
+function needLogin() {
+    ldap = prompt("로그인이 필요합니다. LDAP을 입력해주세요. (꼭 본인 LDAP으로만 로그인 해주세요)");
+    
+}
